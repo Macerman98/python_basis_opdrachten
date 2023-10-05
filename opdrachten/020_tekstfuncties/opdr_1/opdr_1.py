ffab7c0a461894b4396e9651b206f98d42360618
Mees Post <meespost98@gmail.com>
2023-10-05 16:19:20 +0200

    Einstein zei ooit...

--- opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
+++ opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
@@ -8,12 +8,15 @@
 # Zorg dat de volgende zin op het scherm wordt getoond met de print-functie:
 # O mamma mia, wat maak je toch heerlijke pizza's
 
-print()
+Dr_oetker = "O mamma mia, wat maak je toch heerlijke pizza's"
+print(Dr_oetker)
 
 # Gebruik onderstaande variabelen en de print-functie
 # en toon de volgende zin op het scherm: Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
 voornaam = "albert"
 achternaam = "einstein"
+tekst = "zei ooit:"
+Tekst1 = "'Als je niks fout doet, dan kun je ook niks leren!"
 
-
-print()
+leren = (voornaam.title()) + " " + (achternaam.title()) + " " + tekst + " " + Tekst1
+print(leren)
