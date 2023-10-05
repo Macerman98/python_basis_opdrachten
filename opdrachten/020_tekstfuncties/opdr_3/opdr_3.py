21736e2afc78c67b34387e693af3be97c8e1ef93
Mees Post <meespost98@gmail.com>
2023-10-05 17:49:31 +0200

    Kerstbomen

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,22 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
+# Naam student: Mees Post
 # Groep:
 
 # Hier komt je code...
+
+a1 = " " * 4 + "*"
+a2 = " " * 3 + "***"
+a3 = " " * 2 + "******"
+a4 = " " * 1 + "********"
+a5 = " " * 0 + "***********"
+a6 = "*" * 11
+a7 = " " * 3 + "***"
+a8 = " " * 3 + "***"
+a9 = " " * 3 + "***"
+
+kerstboom = (a1, a2 ,a3, a4, a5, a6, a7, a8, a9) 
+
+for hoeveelheid in range(5):
+    for Mees in kerstboom:
+        print(Mees)
+    print()
