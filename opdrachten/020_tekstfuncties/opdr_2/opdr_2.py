22bdf1aa4ce0eafa6a71b009802de446a53f9091
Mees Post <meespost98@gmail.com>
2023-10-05 16:59:04 +0200

    Letters tellen

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,10 @@
 
 # Hier komt je code...
 
-my_list = ...
+my_list = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+#Zorg ervoor dat de volgende zin op het scherm verschijnt:
+#De letter "t" komt 10x voor in my_string
 
-print()
+y = my_list.count("t")
+output = f'De letter "t" komt {y}x voor in my_string'
+print(output)
