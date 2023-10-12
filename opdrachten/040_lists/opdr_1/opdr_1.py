42be8468aee9fee5ec19c4f9612dd2b6a2f2d081
Mees Post <meespost98@gmail.com>
2023-10-12 17:58:44 +0200

    Voornaam + achternaam

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,12 @@
 # Opdracht 1 lists
-# Naam student:
+# Naam student: Mees Post
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+dict_1 = {"ID": 0, "Voornaam": "Mees", "Achternaam": "Post"}
+dict_2 = {"ID": 1, "Voornaam": "Bart", "Achternaam": "Vergeer"}
+dict_3 = {"ID": 2, "Voornaam": "Klaas", "Achternaam": "Regie"}
+dict_4 = {"ID": 3, "Voornaam": "Lisa", "Achternaam": "de Wit"}
+
+mylist = (dict_1, dict_2, dict_3, dict_4)
+
+print(mylist[3],["Voornaam"], mylist[3]["Achternaam"])
