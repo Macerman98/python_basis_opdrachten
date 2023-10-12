8d64f92eb8168e0937c88d5e8281e4199572a3da
Mees Post <meespost98@gmail.com>
2023-10-12 16:28:58 +0200

    Opdrachten

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,19 @@
 # Opdracht 1 berekeningen
-# Naam student:
+# Naam student: Mees Post
 # Groep:
 
 # Hier komt je code...
+Jimmy = 23
+Thei = 22
+Luca = 22
+
+Vrienden = (Jimmy, Thei, Luca)
+
+#optellen leeftijd
+opgetelde_leeftijd = sum(Vrienden)
+#gemiddelde leeftijd
+Gemiddelde_leeftijd = opgetelde_leeftijd / 3
+
+print("Dit weten we over joe, bob en fred:")  
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =", opgetelde_leeftijd)
+print("De gemiddelde leeftijd van deze boys is:", Gemiddelde_leeftijd)
