33ef515962e14c02f8df67c08f552d5603f93306
Mees Post <meespost98@gmail.com>
2023-10-14 20:51:35 +0200

    Stelling van Pythagoras

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,10 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+eerste_vraag = int(input("Geef de lengte van de eerste zijde: \n"))
+tweede_vraag = int(input("Geef de lengte van de tweede zijde: \n"))
 
+uitkomst = (eerste_vraag**2 + tweede_vraag**2)
+import math
+wortel = (math.sqrt(uitkomst))
+print(f"De lengte van de schuine zijde is: {wortel:.0f}")
