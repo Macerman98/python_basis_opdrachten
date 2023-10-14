839c2f44587095b6272598f73deba82d6c5e5d74
Mees Post <meespost98@gmail.com>
2023-10-14 23:12:36 +0200

    Voetballers

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -3,4 +3,8 @@
 # Groep:
 
 # Hier komt je code...
+object_list = input("Noem vijf grote voetballers: \n ")
+voetballers = object_list.split(', ')
 
+voetballers.sort(key=lambda x: x.lower(), reverse=True)
+print(voetballers)
