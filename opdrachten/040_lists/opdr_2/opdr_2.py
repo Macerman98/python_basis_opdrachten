b56c3348b0cb5dde66b1a830057219e8628a35a6
Mees Post <meespost98@gmail.com>
2023-10-14 21:56:33 +0200

    Thuisblijvers

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -12,14 +12,4 @@ rivier_info = {
 rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
-eerste_rivier = rivieren[0].capitalize()
-Tweede_land = rivier_info[rivieren[0]][1].capitalize()
-print("De rivier " + eerste_rivier + " stroomt onder andere door " + Tweede_land)
-
-tweede_rivier = rivieren[1].capitalize()
-eerste_land = rivier_info[rivieren[0]][1].capitalize()
-print("De rivier " + tweede_rivier + " stroomt onder andere door " + eerste_land)
-
-derde_rivier = rivieren[2].capitalize()
-derde_land = rivier_info[rivieren[2]][0].capitalize()
-print("De rivier " + derde_rivier + " stroomt onder andere door " + derde_land)
+print(rivieren[0], ["duitsland"])
--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -3,5 +3,25 @@
 # Groep:
 
 # Hier komt je code...
+gasten = ["Mees", "Paul", "Kees", "Marie", "Hilda"]
+print(gasten)
 
-gasten = ["Jij", ... ]
+# vraag of er nog mensen thuis blijven:
+thuisblijvers = input("Wie blijft er thuis? \n")
+if thuisblijvers in gasten:
+    gasten.remove(thuisblijvers)
+else:
+    print(f"{thuisblijvers} staat niet op de gastenlijst")
+print("bijgewerkte gastenlijst:")
+print(gasten)
+
+# vraag of er nog nieuwe mensen mee gaan:
+nieuwe_gast = input("Gaan er nog nieuwe mensen mee? \n")
+
+# nieuwe gast naar Kees:
+if nieuwe_gast not in gasten:
+    gasten.insert(3, nieuwe_gast)
+else:
+    print(f"{nieuwe_gast} staat al op de gastenlijst")
+print("bijgewerkte gastenlijst:")
+print(gasten)
