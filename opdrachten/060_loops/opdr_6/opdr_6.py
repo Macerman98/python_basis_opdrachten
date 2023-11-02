343744ab3d91c676b7df71a1def4dde159dd5d8f
Mees Post <meespost98@gmail.com>
2023-11-02 12:36:50 +0100

    Tweede vraag

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -9,5 +9,21 @@
 pizzas = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
 
 #sorteren van de pizza's
-sorteren = sorted(pizzas)
-print(sorteren)
+sorted_pizza = sorted(pizzas)
+print(sorted_pizza)
+print("\n")
+
+#toevoegen pizza
+sorted_pizza.append("zalm")
+print(sorted_pizza)
+print("\n")
+
+del sorted_pizza[3]
+print(sorted_pizza)
+print("\n")
+
+#printen eerste drie pizza's
+print(sorted_pizza[0:3])
+
+gemiddelde = len(sorted_pizza) // 2
+print(gemiddelde)
