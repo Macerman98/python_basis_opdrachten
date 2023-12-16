dd5f1c91144ec76c41a0e0f48377b2b582d52596
Mees Post <meespost98@gmail.com>
2023-12-16 15:06:58 +0100

    Opdrachten

--- opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
+++ opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
@@ -4,3 +4,40 @@
 
 
 # Party enquete
+
+def vraag_invoer(vraag_nummer, vraag):
+    antwoord = input(f"{vraag_nummer}. {vraag}\n")
+    return antwoord
+
+def main():
+    vragen = [
+        "Wat is je voornaam?",
+        "Wat is je achternaam?",
+        "Wat neem je mee aan drank?",
+        "Wat neem je mee om te eten?"
+    ]
+
+    feestgangers = []
+
+    for i, vraag in enumerate(vragen, 1):
+        antwoord = vraag_invoer(i, vraag)
+        feestgangers.append(antwoord)
+
+    # Maak een dictionary van de antwoorden
+    feestganger_dict = {
+        "voornaam": feestgangers[0],
+        "achternaam": feestgangers[1],
+        "drank": feestgangers[2],
+        "eten": feestgangers[3]
+    }
+
+    # Schrijf de antwoorden naar een tekstbestand
+    with open("feestgangers.txt", "a") as bestand:
+        bestand.write("----\n")
+        for key, value in feestganger_dict.items():
+            bestand.write(f"{key}: {value}\n")
+
+    print("\nBedankt voor het invullen!\nSee you at the party.")
+
+if __name__ == "__main__":
+    main()
--- opdrachten/090_functies/opdr_2/opdr_2.py
+++ opdrachten/090_functies/opdr_2/opdr_2.py
@@ -3,18 +3,20 @@
 # Groep:
 
 
-def kilometers_naar_miles(km):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+def kilometers_naar_miles(kilometers):
+    miles = kilometers / 1.609344
+    return miles
 
 def miles_naar_kilometers(miles):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    kilometers = miles * 1.609344
+    return kilometers
 
+# Voorbeeld van het gebruik van de functies
 kilometers = 1223
 miles = 867
 
-print(kilometers_naar_miles(kilometers))
-print(miles_naar_kilometers(miles))
+miles_berekening = kilometers_naar_miles(kilometers)
+kilometers_berekening = miles_naar_kilometers(miles)
+
+print(f"{kilometers} kilometers = {miles_berekening} miles")
+print(f"{miles} miles = {kilometers_berekening} kilometers")
