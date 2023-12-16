d088820d94b3b46b11256a9a7830d15e53602855
Mees Post <meespost98@gmail.com>
2023-12-16 15:45:23 +0100

    Verder afgerondd

--- opdrachten/090_functies/opdr_3/opdr_3.py
+++ opdrachten/090_functies/opdr_3/opdr_3.py
@@ -1,20 +1,24 @@
 # Opdracht 1 functies
 # Naam student:
-# Groep:
+# Groep: Mees Post
 
+import math
 
-def kubus_vol(m):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+def kubus_vol(zijde):
+    volume = zijde**3
+    return volume
 
-def bol_vol(r):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+def bol_vol(radius):
+    volume = (4/3) * math.pi * radius**3
+    return volume
 
-zijde = 5
-radius = 4
+# voorbeeld vanuit uitleg
+zijde_kubus = 100
+radius_bol = 50
 
-print(kubus_vol(5))
-print(bol_vol(4))
+volume_kubus = kubus_vol(zijde_kubus)
+volume_bol = bol_vol(radius_bol)
+
+print(f"De inhoud van deze kubus is: {volume_kubus}")
+print(f"De inhoud van deze bol is: {volume_bol}")
+
--- opdrachten/090_functies/opdr_4/opdr_4.py
+++ opdrachten/090_functies/opdr_4/opdr_4.py
@@ -1,14 +1,22 @@
 # Opdracht 1 functies
 # Naam student:
-# Groep:
-
+# Groep: Mees Post
 
 def volledige_naam(lijst_met_namen):
-    # hier komt jouw code
-    # Het woordje pass mag je weghalen
-    pass
+    for persoon in lijst_met_namen:
+        # Bouw de volledige naam op
+        volledige_naam = persoon["voornaam"]
+        if persoon["tussenvoegsel"]:
+            volledige_naam += f" {persoon['tussenvoegsel']}"
+        volledige_naam += f" {persoon['achternaam']}"
 
+        # Verwijder dubbele spaties
+        volledige_naam = ' '.join(volledige_naam.split())
 
+        # Print de volledige naam
+        print(volledige_naam)
+
+# Gegeven lijst met namen
 namen = [
     {"voornaam": "Willem", "tussenvoegsel": "van", "achternaam": "Dijk"},
     {"voornaam": "Klaas", "tussenvoegsel": "", "achternaam": "Wopstra"},
@@ -16,4 +24,5 @@ namen = [
     {"voornaam": "Carla", "tussenvoegsel": "", "achternaam": "Hoogvliet"},
 ]
 
+# Roep de functie aan met de gegeven lijst met namen
 volledige_naam(namen)
